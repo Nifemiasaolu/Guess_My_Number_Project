9603fbbf6d76d26ff17eafb2acba95d30aac207f
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-05-03 11:26:12 +0100

    Implementing the game logic

--- script.js
+++ script.js
@@ -13,12 +13,39 @@
 ////////////////
 // Handling Click Events
 
+// Defining the Secret Number
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+document.querySelector('.number').textContent = secretNumber;
+
+// Defining the score variable, and assigning it a value.
+let score = 20;
+
+// Functionalizing the Check button
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
-  if(!guess) {
+  if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number';
-
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
