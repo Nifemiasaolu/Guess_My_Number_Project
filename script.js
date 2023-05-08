f0452184dbd3f74e1d46e3824443a18a3c66b84d
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-05-08 12:20:46 +0100

    New details

--- script.js
+++ script.js
@@ -136,3 +136,31 @@
 // });
 
 
+let secretNumber = Math.trunc(Math.random () * 20 + 1);
+// document.querySelector ('.number').textContent = secretNumber;
+let score = 20;
+let highscore;
+
+document.querySelector('.check').addEventListener('click', function (){
+  const guess = Number ((document.querySelector('.guess').value));
+  // console.log(guess, typeof guess);1
+  
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number';
+  } else if (guess > 20){
+    document.querySelector('.message').textContent = 'Number is above 20!';
+  } else if(guess === secretNumber){
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too high!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too low!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+})
