f4b4d1978be26bb2285b9bcccc0aa049bc317c1e
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-05-08 10:50:55 +0100

    Script transformed

--- script.js
+++ script.js
@@ -14,83 +14,83 @@
 // Handling Click Events
 
 // Defining the Secret Number
-let secretNumber = Math.trunc(Math.random() * 20 + 1);
+// let secretNumber = Math.trunc(Math.random() * 20 + 1);
 
-// Defining the score variable, and assigning it a value.
-let score = 20;
+// // Defining the score variable, and assigning it a value.
+// let score = 20;
 
-// Defining the highscore
-let highscore = 0;
+// // Defining the highscore
+// let highscore = 0;
 
-// Refactoring the document functions
+// // Refactoring the document functions
 
-const displayMessage = function (message) {
-  document.querySelector('.message').textContent = message;
-};
-const displayScore = function (score) {
-  document.querySelector('.score').textContent = score;
-};
-const displayNumber = function (number) {
-  document.querySelector('.number').textContent = number;
-};
-const displayGuess = function (guess) {
-  document.querySelector('.guess').value = guess;
-};
-const displayBackgroundColor = function (color) {
-  document.querySelector('.body').style.backgroundColor = color;
-};
-const displayNumberWidth = function (width) {
-  document.querySelector('.number').style.width = width;
-};
-// Functionalizing the Check button
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
+// const displayMessage = function (message) {
+//   document.querySelector('.message').textContent = message;
+// };
+// const displayScore = function (score) {
+//   document.querySelector('.score').textContent = score;
+// };
+// const displayNumber = function (number) {
+//   document.querySelector('.number').textContent = number;
+// };
+// const displayGuess = function (guess) {
+//   document.querySelector('.guess').value = guess;
+// };
+// const displayBackgroundColor = function (color) {
+//   document.querySelector('.body').style.backgroundColor = color;
+// };
+// const displayNumberWidth = function (width) {
+//   document.querySelector('.number').style.width = width;
+// };
+// // Functionalizing the Check button
+// document.querySelector('.check').addEventListener('click', function () {
+//   const guess = Number(document.querySelector('.guess').value);
+//   console.log(guess, typeof guess);
 
-  //when there is no input
-  if (!guess) {
-    // document.querySelector('.message').textContent = '⛔ No Number';
-    displayMessage('⛔ No Number');
+//   //when there is no input
+//   if (!guess) {
+//     // document.querySelector('.message').textContent = '⛔ No Number';
+//     displayMessage('⛔ No Number');
 
-    // When Player wins
-  } else if (guess === secretNumber) {
-    // document.querySelector('.message').textContent = '🎉 Correct Number!';
-    displayMessage('🎉 Correct Number!');
+//     // When Player wins
+//   } else if (guess === secretNumber) {
+//     // document.querySelector('.message').textContent = '🎉 Correct Number!';
+//     displayMessage('🎉 Correct Number!');
     
-    // document.querySelector('.number').textContent = secretNumber;
-    displayNumber(secretNumber);
+//     // document.querySelector('.number').textContent = secretNumber;
+//     displayNumber(secretNumber);
 
-    // document.querySelector('body').style.backgroundColor = '#60b347';
-    displayBackgroundColor('#60b347');
+//     // document.querySelector('body').style.backgroundColor = '#60b347';
+//     displayBackgroundColor('#60b347');
 
-    // document.querySelector('.number').style.width = '30rem';
-    displayNumberWidth('30rem');
+//     // document.querySelector('.number').style.width = '30rem';
+//     displayNumberWidth('30rem');
 
-    if (score > highscore) {
-      highscore = score;
-      document.querySelector('.highscore').textContent = highscore;
-    }
-    // When guess passes 20
-  } else if (guess > 20) {
-    // document.querySelector('.message').textContent = 'Number above 20!';
-    displayMessage('Number above 20!');
-    ///////// Refactored Code ////////
-    // When guess is wrong
-  } else if (guess !== secretNumber) {
-    if (score > 1) {
-      // document.querySelector('.message').textContent =
-      // guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
-      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
-      score--;
-      displayScore(score);
+//     if (score > highscore) {
+//       highscore = score;
+//       document.querySelector('.highscore').textContent = highscore;
+//     }
+//     // When guess passes 20
+//   } else if (guess > 20) {
+//     // document.querySelector('.message').textContent = 'Number above 20!';
+//     displayMessage('Number above 20!');
+//     ///////// Refactored Code ////////
+//     // When guess is wrong
+//   } else if (guess !== secretNumber) {
+//     if (score > 1) {
+//       // document.querySelector('.message').textContent =
+//       // guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
+//       displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+//       score--;
+//       displayScore(score);
 
-      // document.querySelector('.message').textContent = 'Number above 20!';
-    } else {
-      // document.querySelector('.message').textContent = '💥 You lost the game';
-      displayMessage('💥 You lost the game');
-      displayScore(0);
-    }
-  }
+//       // document.querySelector('.message').textContent = 'Number above 20!';
+//     } else {
+//       // document.querySelector('.message').textContent = '💥 You lost the game';
+//       displayMessage('💥 You lost the game');
+//       displayScore(0);
+//     }
+//   }
 
   ///////////Detailed Code /////////////
 
@@ -118,7 +118,7 @@ document.querySelector('.check').addEventListener('click', function () {
   //     document.querySelector('.score').textContent = 0;
   //   }
   // }
-});
+// });
 
 // Functionalizing the again button
 
@@ -134,3 +134,5 @@ document.querySelector('.check').addEventListener('click', function () {
 //   displayBackgroundColor('#222');
 //   displayNumberWidth('15rem');
 // });
+
+
